f91c7fa640647dfcc0cffd56b37663e32d6b9c9d
wspisak <wojtek.spisak@gmail.com>
2020-12-05 12:55:57 +0100

    Initial commit

---
+++ src/main/java/pl/sda/app_event/AppEventApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.app_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/app_event/AppEventApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.app_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
